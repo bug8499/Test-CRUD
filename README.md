0052c298625d3727b63d632ab377b2d591020d64
bug8499 <bug8499@gmail.com>
2026-03-15 21:52:01 +0700

    save1

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-Hi
+Project setup instructions.
+1.
+2. สามารถดู Database ได้จาก pgadmin4 ที่ localhost:5050
+
+Key architectural
+1. ใช้ Postgresql เป็น Database
+2. Front-End ใช้ React Vite
+3. Back-End ใช้ Nest.js
+4. ส่วนของ ORM ใช้ Sequlize ในการติดต่อใช้งาน Database
--- task-frontend/src/App.tsx
+++ task-frontend/src/App.tsx
@@ -1,21 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login'
 import Task from './pages/Tasks'
-import Test from './pages/Test'
 
 function App() {  
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login/>} />
         <Route path="/tasks" element={<Task/>}/>
-        <Route path="/test" element={<Test/>}/>
       </Routes>
     </BrowserRouter>
   )
